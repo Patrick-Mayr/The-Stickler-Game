39dc01b23ab6d4743a1778be70918d806dc34fd1
Patrick-Mayr <Patrick.mayr@ontariotechu.net>
2024-02-12 20:16:48 -0500

    fixed sprint and added staminas

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,19 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] float speed;
-    [SerializeField] float sprintSpeed; 
+    [SerializeField] float sprintSpeed;
+    [SerializeField] float airSpeed;
     [SerializeField] float acceleration;
     [SerializeField] float jumpHeight;
+    [SerializeField] float stamina;
+    [SerializeField] float staminaDepletion;
+    [SerializeField] float staminaRecovery;
     [SerializeField] private LayerMask groundLayer;
     
     Rigidbody2D rb;
     private Vector2 moveDirection;
     private bool isSprinting = false;
+    float staminaAmount;
     
 
     // Start is called before the first frame update
@@ -24,30 +29,47 @@ public class PlayerMovement : MonoBehaviour
         Manager.Init(this);
         Manager.SetGameControls();
         rb = GetComponent<Rigidbody2D>();
-        
+        staminaAmount = stamina;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isSprinting)
+        if (!isSprinting && IsGrounded())
         {
             Vector2 targetVelocity = new Vector2(moveDirection.x, 0.0f) * speed;
             rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, targetVelocity.x, acceleration * Time.deltaTime), rb.velocity.y);
             rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -speed, speed), rb.velocity.y);
         }
-        else
+        else if (isSprinting && IsGrounded()) 
         {
             Vector2 targetVelocity = new Vector2(moveDirection.x, 0.0f) * sprintSpeed;
             rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, targetVelocity.x, acceleration * Time.deltaTime), rb.velocity.y);
             rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -sprintSpeed, sprintSpeed), rb.velocity.y);
+        } else if (!isSprinting && !IsGrounded())
+        {
+            Vector2 airMove = new Vector2 (moveDirection.x,0) * airSpeed ;
+            rb.AddForce(airMove);
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -speed, speed), rb.velocity.y);
+        } 
+        else
+        {
+            Vector2 airMove = new Vector2(moveDirection.x, 0) * airSpeed;
+            rb.AddForce(airMove);
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -sprintSpeed, sprintSpeed), rb.velocity.y);
         }
-       
     }
 
     public void SetMovementDirection(Vector2 currentDirection)
     {
-        moveDirection = currentDirection;
+        moveDirection = currentDirection; 
+        if (currentDirection.x > 0)
+        {
+            transform.localScale = new Vector3 (0.25f ,0.25f ,0.25f);
+        }else if (currentDirection.x < 0)
+        {
+            transform.localScale = new Vector3(-0.25f, 0.25f, 0.25f);
+        }
     }
 
     public void Jump()
@@ -61,13 +83,41 @@ public class PlayerMovement : MonoBehaviour
 
     public void Sprint()
     {
-        
-        isSprinting = true;
+        if (IsGrounded())
+        {
+            isSprinting = true;
+            StartCoroutine(StaminaBar());
+        }
     }
     public void StopSprint()
     {
         
         isSprinting = false;
+    }
+
+    public IEnumerator StaminaBar()
+    {
+        
+        while (isSprinting && staminaAmount > 0f)
+        {
+            yield return new WaitForSeconds(staminaDepletion);
+            staminaAmount--;
+           //staminaAmount = Mathf.Clamp(staminaAmount, 0f, stamina);
+            Debug.Log("Deplete: " + staminaAmount.ToString());
+        } 
+
+        if (staminaAmount == 0f)
+        {
+            isSprinting = false;
+        }   
+        while (!isSprinting && staminaAmount < stamina)
+        {          
+            yield return new WaitForSeconds(staminaRecovery);
+            staminaAmount++;
+            //staminaAmount = Mathf.Clamp(staminaAmount, 0f, stamina);
+            Debug.Log("Recovery: " + staminaAmount.ToString());
+        } 
+        yield return null;
     }
 
     private bool IsGrounded()
@@ -76,12 +126,12 @@ public class PlayerMovement : MonoBehaviour
         
         if (hit.collider != null)
         {
-            Debug.Log("Grounded");
+            //Debug.Log("Grounded");
             return true;
         }
         else
         {
-            Debug.Log("Not Grounded");
+            //Debug.Log("Not Grounded");
             return false;
         }
     }
